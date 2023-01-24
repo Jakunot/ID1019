77eb95bb1b7c936717118c9eaf805ebc3b513f5b
jkotieno <jkotieno@kth.se>
2023-01-24 17:16:58 +0100

    simple functions

---
+++ simple-functions/simple-functions.ex
@@ -0,0 +1,18 @@
+defmodule Test do
+
+    #simple function to add x with 5
+    def add5(x) do
+        x + 5
+    end
+
+    #simple function to add x with y
+    def addxy(x, y) do
+        x + y
+    end
+
+    #simple function to convert celsius to fahrenheit
+    def to_fahren(c) do
+        c * 1.8 + 32
+    end
+    
+end
